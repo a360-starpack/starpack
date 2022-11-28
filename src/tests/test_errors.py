2014b0335fb471804b4369353cc7dc83bf55bd3a
Irvin Shen <irvin.shen@andromeda360.ai>
2022-11-28 15:15:07 -0600

    Added tests for error initialization

---
+++ src/tests/test_errors.py
@@ -0,0 +1,42 @@
+import pytest
+from pathlib import Path
+from starpack import errors
+
+
+def test_docker_not_found_error() -> None:
+    with pytest.raises(errors.DockerNotFoundError) as e:
+        raise errors.DockerNotFoundError()
+
+    assert e.value.exit_code == 1
+
+
+def test_docker_not_found_error() -> None:
+    with pytest.raises(errors.DockerNotFoundError) as e:
+        raise errors.DockerNotFoundError()
+
+    assert e.value.exit_code == 1
+
+
+def test_local_only_error() -> None:
+    with pytest.raises(errors.LocalOnlyError) as e:
+        raise errors.LocalOnlyError()
+
+    assert e.value.exit_code == 1
+
+
+def test_path_exists_error(capsys) -> None:
+    with pytest.raises(errors.PathExistsError) as e:
+        path = Path()
+        raise errors.PathExistsError(path)
+
+    output = capsys.readouterr()
+
+    assert str(path) in output.out
+    assert e.value.exit_code == 1
+
+
+def test_engine_initialization_error() -> None:
+    with pytest.raises(errors.EngineInitializationError) as e:
+        raise errors.EngineInitializationError()
+
+    assert e.value.exit_code == 1
